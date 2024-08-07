0ac8c26edfab1957a3884a4e5020760bf80c63c4
Gulezahra-Haider <gulezahraahaider@gmail.com>
2024-08-07 17:22:11 +0500

    updated app and cpp without print

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int main()
     auto sum = [] ( int a, int b) { return (a+b); };
     
     int sum1 = sum(5,6);
-    cout << "Sum is " << sum1 << endl;
+    //cout << "Sum is " << sum1 << endl;
 
     
   return 0;
