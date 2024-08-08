b51450e2ab521723dcd5a388f5eb639508953391
Gulezahra-Haider <gulezahraahaider@gmail.com>
2024-08-08 11:56:23 +0500

    updated test.cpp for vectors, and generated new app

--- test.cpp
+++ test.cpp
@@ -6,10 +6,23 @@ using namespace std;
 int main() 
 {
     
-    auto sum = [] ( int a, int b) { return (a+b); };
-    
-    int sum1 = sum(20,30);
-    cout << "Sum is " << sum1 << endl;
+  auto sum = [] ( int a, int b) { return (a+b); };
+  
+  int sum1 = sum(20,30);
+  cout << "Sum is " << sum1 << endl;
+
+
+  vector<int> vect1;
+  vect1.push_back(3);
+  vect1.push_back(40);
+  vect1.push_back(50);
+
+  vector<int> :: iterator vect1_iter;
+  for( vect1_iter=vect1.begin() ; vect1_iter!=vect1.end() ; vect1_iter++ )
+  {
+    cout << *(vect1_iter) << " ";
+  }
+
 
     
   return 0;
