85f58b99a27bf2af5e405bab52beac2401c0c913
Gulezahra-Haider <gulezahraahaider@gmail.com>
2024-08-15 16:05:44 +0500

    modified test.cpp for vector 2

--- test.cpp
+++ test.cpp
@@ -12,11 +12,13 @@ int main()
   // cout << "Sum is " << sum1 << endl;
 
 
-  vector<int> vect1;
+  vector<int> vect1, vect2;
   vect1.push_back(3);
   vect1.push_back(40);
   vect1.push_back(50);
 
+  cout << "Printing Vector 1" << endl;
+  
   vector<int> :: iterator vect1_iter;
   for( vect1_iter=vect1.begin() ; vect1_iter!=vect1.end() ; vect1_iter++ )
   {
@@ -26,7 +28,17 @@ int main()
   cout << "HELLO I AM HERE" << endl;
   cout << "testing checkout commit ids" << endl;
 
-    
+  vect2.push_back(5);
+  vect2.push_back(6);
+  vect2.push_back(7);
+  
+  cout << "Printing Vector 2" << endl;
+  vector<int> :: iterator vect2_iter;
+  for( vect2_iter=vect2.begin() ; vect2_iter!=vect2.end() ; vect2_iter++ )
+  {
+	  cout << *(vect2_iter) << " ";
+  }
+  
   return 0;
 }
 
